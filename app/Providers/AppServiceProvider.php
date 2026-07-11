34ea9b228fa02f031541ebdc0194505ddd5d7524
Luong Tue Nhi <luongtuenhi2021nn@gmail.com>
2026-07-11 13:28:15 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,8 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production' || app()->environment('production')) {
+        if (!App::runningInConsole()) {
         URL::forceScheme('https');
     }
     }
+    
 }
